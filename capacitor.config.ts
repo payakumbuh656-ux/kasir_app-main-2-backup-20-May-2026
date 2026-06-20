cd661df02c0c809919449d63b687360502b88a22
payakumbuh656-ux <payakumbuh656@gmail.com>
2026-06-21 06:27:13 +0800

    Android APK build successful and Firebase Android configured

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.indotech.pos',
+  appName: 'IndoTech POS',
+  webDir: 'dist-react'
+};
+
+export default config;

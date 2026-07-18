e69dcc40911d0700eef71be638132f840cbe43d3
payakumbuh656-ux <payakumbuh656@gmail.com>
2026-07-19 04:22:21 +0700

    v0.9.1 - Electron foundation

---
+++ electron/main.cjs
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (!app.isPackaged) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist-react/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
